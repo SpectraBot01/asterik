d7450dd09238d51d5dc31fcaddd69adc31058588
lafuga0112 <50000949+lafuga0112@users.noreply.github.com>
2026-02-12 14:01:25 -0400

    hgfghgfh

--- src/config/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Índice de configuraciones centralizadas
- * Exporta todas las configuraciones de los diferentes módulos
- */
-
-export { TrunkConfig } from './trunk-config';
-export { ARIConfig } from './ari-config';
-export { CampaignConfig } from './campaign-config'; 
